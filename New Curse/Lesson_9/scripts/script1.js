7c3e059abb6ab7b868e5c4924fb935cd7d009fb5
alexykr alexykr <sumskaqqq@gmail.comgit config --global user.name Oleksii>
2023-02-09 12:42:41 +0100

    Les9

---
+++ New Curse/Lesson_9/scripts/script1.js
@@ -0,0 +1,57 @@
+// console.log("Hello world");
+
+// вывод на консоль - concatenation:
+// const name = prompt("Writte your name");
+// console.log("Hello " + name);
+//promt исп. только для проверки, с html не исп!
+
+//можно таким способом выводить на консоль - interpolation:
+// console.log(`Hello ${name}! How are you?`)
+
+// Символы в java script:
+// A-Z a-z $ 0-9
+
+// комбинации символов:
+// a1, $a, a$, car_name,  carName - так можно!
+// 1a, car name, car-name - нельзя так!
+
+// объявление переменных:
+// let - исп. тогда когда перем. может менять значение
+// const - исп. тогда когда перем. не меняет значение
+// var - не исп.
+
+// console.log(5) //string
+// console.log("5") // number
+
+// console.log(typeof(name)) // string
+// console.log(typeof(5))// number
+// console.log(typeof name) // string
+// console.log(typeof 5) // number
+
+// const r = 12
+// const g = 43
+// const b = 14
+
+// console.log("rgb(" + r + ", " + g + ", " + b + ");") //concatenation
+// console.log(`rgb(${r}, ${g}, ${b})`) //interpolation
+
+//матем. операции:
+const value_1 = 13
+const value_2 = 4
+
+console.log(value_1 + value_2) //сложение
+console.log(value_1 - value_2) //вычитание
+console.log(value_1 * value_2) //умножение
+console.log(value_1 / value_2) //деление
+console.log(value_1 % value_2) //отсаток от деления
+console.log(value_1 ** value_2) //возв. в степень
+
+
+
+
+
+
+
+
+
+
---
+++ New Curse/Lesson_9/scripts/script2.js
@@ -0,0 +1,68 @@
+//методы преобраз. строки в число и наоборот:
+// в консоли фиолетовый - число, белый - строка 
+// console.log("-------------------------------");
+// let a = 2 // number
+// let b = "2" //string
+// console.log(String(a)) //число в строку "2"
+// console.log(Number(b)) //строку в число 2
+// //или так еще короче можно:
+// console.log(+b) //строку в число 2
+// console.log("" + a) //число в строку "2"
+
+// console.log("2" + 2); //число в строку "22"
+// console.log("2" - 2); //строку в число 2 - 2 = 0
+
+// console.log("-------------------------------");
+
+// console.log(Number("a"))
+// console.log(typeof(NaN));
+
+// Програма введенный возраст в минутах:
+// const age = prompt("Writte your age");
+// console.log("Your age in min - " + age * 525960);
+
+//Прграмма возв. в квадрат введенное число:
+// const number = prompt("Writte your number");
+// console.log("Your result is - " + number**2);
+
+//еще програма с математикой:
+// const num_1 = prompt("Write first number")
+// const num_2 = prompt("Write second number")
+
+// console.log(`num_1 + num_2 = ${+num_1 + +num_2}`);
+
+const a ="2"
+const b =2
+
+if (a == b) { //автом. стр. превращ. в число здесь
+    console.log("a == b")
+} else {
+    console.log("a != b");
+} 
+
+if (a>b) {
+    console.log("a больше b");
+} else {
+    console.log("a меньше b");
+}
+
+if (a <= b) {
+    console.log("a меньше или равно b");
+} else {
+    console.log("a больше или равно b");
+}
+
+if (a != b) {
+    console.log("a не равно b");
+    
+} else {
+    console.log("a равно b");
+}
+
+if (a !== b) {
+    console.log("тип a не равно b");
+    
+} else {
+    console.log("тип a равно b");    
+}
+
