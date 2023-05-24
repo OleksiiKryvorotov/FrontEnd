7824ac1685f0b028232ad8faf898512b677b2ce0
Oleksii.Kryvorotov <sumskaqqq@gmail.com>
2023-05-24 12:42:43 +0200

    react1

--- ProfKurs/Cl_W/Lesson_20_react/myapp/src/App.js
+++ ProfKurs/Cl_W/Lesson_20_react/myapp/src/App.js
@@ -1,12 +1,52 @@
 import './App.css';
+import User from './components/User';
+
 
 function App() {
+
+  const users = [
+    {
+      id: 1,
+      firstname: 'Oleg',
+      lastname: 'Petrov',
+      age: 30
+    },
+    {
+      id: 2,
+      firstname: 'Anna',
+      lastname: 'Sidorova',
+      age: 22
+    },
+    {
+      id: 3,
+      firstname: 'Ivan',
+      lastname: 'Gribov',
+      age: 13
+    },
+    {
+      id: 4,
+      firstname: 'Irina',
+      lastname: 'Sokolova',
+      age: 54
+    },
+    {
+      id: 5,
+      firstname: 'Nelli',
+      lastname: 'Efremyan',
+      age: 28
+    }
+  ]
+
   return (
     <div>
-  Все работает
-  <p></p>
-    </div>   
-
+      {
+        users
+        //показывать только тех польз., которые старше 18
+        .filter(el => el.age >= 18)
+        //---------------------------------------------
+        .map(el => <User  firstname={el.firstname} lastname={el.lastname} age={el.age} key={el.id} />)
+      }         
+    </div>  
   );
 }
 
