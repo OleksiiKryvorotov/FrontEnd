f285608cadf9edbf24fd949b9b31a40130ba0a29
Oleksii.Kryvorotov <sumskaqqq@gmail.com>
2023-05-26 12:50:28 +0200

    telegram

---
+++ ProfKurs/Practic/CW_pracric/Les_17/myapp/src/components/Menu/Menu.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import'./Menu.css'
+
+export default function Menu() {
+  return (
+    <div className='MenuIcon_wrapper'><i className="MenuIcon fa-solid fa-bars"></i>
+    </div>
+  )
+}
---
+++ ProfKurs/Practic/CW_pracric/Les_17/myapp/src/components/SearchChats/SearchChats.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './SearchChats.css'
+
+export default function SearchChats() {
+  return (
+    <div className='SearchChats'>
+      <i className='SearchChats_icon fa-solid fa-magnifying-glass'></i>
+      <input className='SearchChats_input' placeholder='Search' />
+
+    </div>
+  )
+}
+
+
