e0943d632cae220d35537aab236e8096ac70859d
Oleksii.Kryvorotov <sumskaqqq@gmail.com>
2023-06-06 09:41:21 +0200

    pr_05_06

--- ProfKurs/Practic/CW_pracric/Les_17/myapp/src/App.js
+++ ProfKurs/Practic/CW_pracric/Les_17/myapp/src/App.js
@@ -2,173 +2,12 @@ import "./App.css";
 import ChatsHeader from "./components/ChatsHeader/ChatsHeader";
 import ChatElement from "./components/ChatElement/ChatElement";
 import { useState } from "react";
-
-const chats = [
-  {
-    id: 1,
-    avatar:
-      "https://amiel.club/uploads/posts/2022-03/1647766434_9-amiel-club-p-kartinki-chat-10.png",
-    title: "Chat number 1",
-    description: "aelr aketkhjanetkhalkntehklslkrlhms;lrm ;mrljmdl;tymj",
-    last_msg_time: "23:59",
-  },
-  {
-    id: 2,
-    avatar:
-      "https://cdn.britannica.com/89/164789-050-D6B5E2C7/Barack-Obama-2012.jpg",
-    title: "Presidents chat",
-    description: "010101010101101010101001001010 0101 010 0101010010",
-    last_msg_time: "01:11",
-  },
-  {
-    id: 3,
-    avatar:
-      "https://amiel.club/uploads/posts/2022-03/1647766434_9-amiel-club-p-kartinki-chat-10.png",
-    title: "Chat number 1",
-    description: "aelr aketkhjanetkhalkntehklslkrlhms;lrm ;mrljmdl;tymj",
-    last_msg_time: "23:59",
-  },
-  {
-    id: 4,
-    avatar:
-      "https://cdn.britannica.com/89/164789-050-D6B5E2C7/Barack-Obama-2012.jpg",
-    title: "Presidents chat",
-    description: "010101010101101010101001001010 0101 010 0101010010",
-    last_msg_time: "01:11",
-  },
-  {
-    id: 5,
-    avatar:
-      "https://cdn.britannica.com/89/164789-050-D6B5E2C7/Barack-Obama-2012.jpg",
-    title: "Presidents chat",
-    description: "010101010101101010101001001010 0101 010 0101010010",
-    last_msg_time: "01:11",
-  },
-  {
-    id: 6,
-    avatar:
-      "https://amiel.club/uploads/posts/2022-03/1647766434_9-amiel-club-p-kartinki-chat-10.png",
-    title: "Chat number 1",
-    description: "aelr aketkhjanetkhalkntehklslkrlhms;lrm ;mrljmdl;tymj",
-    last_msg_time: "23:59",
-  },
-  {
-    id: 7,
-    avatar:
-      "https://cdn.britannica.com/89/164789-050-D6B5E2C7/Barack-Obama-2012.jpg",
-    title: "Presidents chat",
-    description: "010101010101101010101001001010 0101 010 0101010010",
-    last_msg_time: "01:11",
-  },
-  {
-    id: 8,
-    avatar:
-      "https://amiel.club/uploads/posts/2022-03/1647766434_9-amiel-club-p-kartinki-chat-10.png",
-    title: "Chat number 1",
-    description: "aelr aketkhjanetkhalkntehklslkrlhms;lrm ;mrljmdl;tymj",
-    last_msg_time: "23:59",
-  },
-  {
-    id: 9,
-    avatar:
-      "https://cdn.britannica.com/89/164789-050-D6B5E2C7/Barack-Obama-2012.jpg",
-    title: "Presidents chat",
-    description: "010101010101101010101001001010 0101 010 0101010010",
-    last_msg_time: "01:11",
-  },
-  {
-    id: 10,
-    avatar:
-      "https://cdn.britannica.com/89/164789-050-D6B5E2C7/Barack-Obama-2012.jpg",
-    title: "Presidents chat",
-    description: "010101010101101010101001001010 0101 010 0101010010",
-    last_msg_time: "01:11",
-  },
-  {
-    id: 11,
-    avatar:
-      "https://amiel.club/uploads/posts/2022-03/1647766434_9-amiel-club-p-kartinki-chat-10.png",
-    title: "Chat number 1",
-    description: "aelr aketkhjanetkhalkntehklslkrlhms;lrm ;mrljmdl;tymj",
-    last_msg_time: "23:59",
-  },
-  {
-    id: 12,
-    avatar:
-      "https://cdn.britannica.com/89/164789-050-D6B5E2C7/Barack-Obama-2012.jpg",
-    title: "Presidents chat",
-    description: "010101010101101010101001001010 0101 010 0101010010",
-    last_msg_time: "01:11",
-  },
-  {
-    id: 13,
-    avatar:
-      "https://amiel.club/uploads/posts/2022-03/1647766434_9-amiel-club-p-kartinki-chat-10.png",
-    title: "Chat number 1",
-    description: "aelr aketkhjanetkhalkntehklslkrlhms;lrm ;mrljmdl;tymj",
-    last_msg_time: "23:59",
-  },
-  {
-    id: 14,
-    avatar:
-      "https://cdn.britannica.com/89/164789-050-D6B5E2C7/Barack-Obama-2012.jpg",
-    title: "Presidents chat",
-    description: "010101010101101010101001001010 0101 010 0101010010",
-    last_msg_time: "01:11",
-  },
-  {
-    id: 15,
-    avatar:
-      "https://cdn.britannica.com/89/164789-050-D6B5E2C7/Barack-Obama-2012.jpg",
-    title: "Presidents chat",
-    description: "010101010101101010101001001010 0101 010 0101010010",
-    last_msg_time: "01:11",
-  },
-  {
-    id: 16,
-    avatar:
-      "https://amiel.club/uploads/posts/2022-03/1647766434_9-amiel-club-p-kartinki-chat-10.png",
-    title: "Chat number 1",
-    description: "aelr aketkhjanetkhalkntehklslkrlhms;lrm ;mrljmdl;tymj",
-    last_msg_time: "23:59",
-  },
-  {
-    id: 17,
-    avatar:
-      "https://cdn.britannica.com/89/164789-050-D6B5E2C7/Barack-Obama-2012.jpg",
-    title: "Presidents chat",
-    description: "010101010101101010101001001010 0101 010 0101010010",
-    last_msg_time: "01:11",
-  },
-  {
-    id: 18,
-    avatar:
-      "https://amiel.club/uploads/posts/2022-03/1647766434_9-amiel-club-p-kartinki-chat-10.png",
-    title: "Chat number 1",
-    description: "aelr aketkhjanetkhalkntehklslkrlhms;lrm ;mrljmdl;tymj",
-    last_msg_time: "23:59",
-  },
-  {
-    id: 19,
-    avatar:
-      "https://cdn.britannica.com/89/164789-050-D6B5E2C7/Barack-Obama-2012.jpg",
-    title: "Presidents chat",
-    description: "010101010101101010101001001010 0101 010 0101010010",
-    last_msg_time: "01:11",
-  },
-  {
-    id: 20,
-    avatar:
-      "https://cdn.britannica.com/89/164789-050-D6B5E2C7/Barack-Obama-2012.jpg",
-    title: "Presidents chat",
-    description: "010101010101101010101001001010 0101 010 0101010010",
-    last_msg_time: "01:11",
-  },
-];
+import UserInfo from "./components/UserInfo/UserInfo";
+import chats from "./mock/chats";
 
 function App() {
   const [currentID, setCurrentId] = useState();
-  console.log(currentID, 'render');
+  console.log(currentID, "render");
 
   return (
     <div className="App">
@@ -192,7 +31,9 @@ function App() {
           })}
         </div>
       </div>
-      <div className="ChatWindow"></div>
+      <div className="ChatWindow">
+        <UserInfo />
+      </div>
     </div>
   );
 }
--- ProfKurs/Practic/CW_pracric/Les_17/myapp/src/components/Menu/Menu.jsx
+++ ProfKurs/Practic/CW_pracric/Les_17/myapp/src/components/Menu/Menu.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
+import "./Menu.css";
 
-import'./Menu.css'
-
-export default function Menu() {
+export default function Menu({ type }) {
   return (
-    <div className='MenuIcon_wrapper'><i className="MenuIcon fa-solid fa-bars"></i>
+    <div className="MenuIcon__wrapper">
+      <i className="MenuIcon fa-solid fa-bars"></i>
     </div>
-  )
+  );
 }
