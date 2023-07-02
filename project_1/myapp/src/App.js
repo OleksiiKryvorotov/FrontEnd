2681dd4645b21dcfd9a7c6a66569262565f06598
Oleksii.Kryvorotov <sumskaqqq@gmail.com>
2023-07-02 20:19:34 +0200

    up2

---
+++ project_1/myapp/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import CostItem from './components/CostItem';
+
+function App() {
+  return (
+    <div>
+       <h1>Изучение реакт</h1>
+       <CostItem />  
+    </div>
+  );
+}
+
+export default App;
