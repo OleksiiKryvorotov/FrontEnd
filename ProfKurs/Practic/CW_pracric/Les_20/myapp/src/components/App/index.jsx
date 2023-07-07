02e4be0e982cc878f6bc864b5063274ef6709332
Oleksii.Kryvorotov <sumskaqqq@gmail.com>
2023-07-07 12:45:11 +0200

    les_20

--- ProfKurs/Practic/CW_pracric/Les_20/myapp/src/components/App/index.jsx
+++ ProfKurs/Practic/CW_pracric/Les_20/myapp/src/components/App/index.jsx
@@ -1,10 +1,16 @@
 import Offers from "../Offers";
 import './style.css'
+import Nav from "../Nav";
+import Footer from "../Footer";
+import Main from "../Main";
 
 function App() {
   return (
     <div>
+      <Nav />
+      <Main />
       <Offers />
+      <Footer />      
     </div>
   );
 }
--- ProfKurs/Practic/CW_pracric/Les_20/myapp/src/components/Offers/index.jsx
+++ ProfKurs/Practic/CW_pracric/Les_20/myapp/src/components/Offers/index.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 import { list } from './data'
 import OfferItem from '../OfferItem'
 import s from './style.module.css'
+import Container from '../UI/Container'
 
 
 export default function Offers() {
   return (
-    <div className={s.offers}>
+    <Container>
         <h2 className={s.h2}>Актуальные спецпредложения</h2>
         <div className={s.offers_list}>
             {
                 list.map(item => <OfferItem  key={item.id} {...item} />)
             }
         </div>
-    </div>
+    </Container >
   )
 }
