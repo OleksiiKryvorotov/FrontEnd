d73e0a590af46063be4c14a678a368dfc24828da
Oleksii.Kryvorotov <sumskaqqq@gmail.com>
2023-07-12 12:43:53 +0200

    routing

---
+++ ProfKurs/Cl_W/routing_fetch/myap/src/pages/SingleProductPage/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { getSingleProducts } from '../../requests/products_req'
+import { useParams } from 'react-router-dom'
+
+export default function SingleProductPage() {
+
+  const { id } = useParams()
+  console.log(id);
+
+  getSingleProducts(id)
+
+  return (
+    <div>SingleProductPag</div>
+  )
+}
