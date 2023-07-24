2aad2f76f8aee6cb22c4256223ba064075f9717b
Oleksii.Kryvorotov <sumskaqqq@gmail.com>
2023-07-24 19:26:19 +0200

    proj1

--- project_1/myapp/src/App.js
+++ project_1/myapp/src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
-import CostItem from './components/CostItem';
+import Costs from './components/Costs';
+
 
 function App() {
+
+     const costs = [
+      {
+      date: new Date(2021, 2, 12) ,
+      description: 'Холодильник',
+      amount: 999.99
+      },
+      {
+      date: new Date(2021, 11, 25) ,
+      description: 'Aquarium',
+      amount: 1254.72
+      },
+      {
+      date: new Date(2021, 3, 1) ,
+      description: 'Джинсы',
+      amount: 49.99
+      }
+    ] 
+
   return (
     <div>
-       <h1>Изучение реакт</h1>
-       <CostItem />  
+       <h1>Изучение реакт</h1> 
+       <Costs costs={costs}/>             
     </div>
   );
 }
--- project_1/myapp/src/components/CostItem.js
+++ project_1/myapp/src/components/CostItem.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import './CostItem.css'
+import CostDate from './CostDate'
+import Card from './Card'
 
-export default function CostItem() {
+export default function CostItem(props) {    
+  
   return ( 
-    <div className='cost-item'>
-      <div>март 2021 12</div>
+    <Card className='cost-item'>
+      <CostDate date={props.date}/>     
         <div className='cost-item__description'>
-          <h2>Холодильник</h2>
-          <div className='cost-item__price'>999.99</div>
+          <h2>{props.description}</h2>
+        <div className='cost-item__price'>${props.amount}</div>
         </div>
-    </div>
+    </Card>
   )
 }
 
